a9d0bdb907fe80a42b4e06cdc9f007a6750596bf
NguyenDinhTuan <tuandinhnguyen2944@gmail.com>
2025-08-09 11:47:48 +0700

    update BookingController và add HistoryBooking

---
+++ DatSanPickleballBE/ModelDto/HistoryBookingDto.cs
@@ -0,0 +1,14 @@
+﻿namespace DatSanPickleballBE.ModelDto
+{
+    public class HistoryBookingDto
+    {
+        public int MaBooking { get; set; }
+        public int MaNguoiDung { get; set; }
+        public string TrangThai { get; set; } = string.Empty;
+        public int MaLichSan { get; set; }
+        public string TenSan { get; set; } = string.Empty;
+        public DateOnly? Ngay { get; set; }
+        public TimeOnly? GioBatDau { get; set; }
+        public TimeOnly? GioKetThuc { get; set; }
+    }
+}
